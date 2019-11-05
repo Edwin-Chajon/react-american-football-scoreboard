0ac4d1ea04c26ae16592ef5016ee25a8f8a4df48
Edwin Chajon <junchajo@gmail.com>
2019-11-04 16:11:43 -0800

    timer stretch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 //TODO: STEP 1 - Import the useState hook.
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
+import Timer from "./Timer";
+
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
@@ -20,13 +22,12 @@ function App() {
 
             <div className="home__score">{countHome}</div>
           </div>
-          <div className="timer">00:00</div>
+          <div className="timer"><Timer/></div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
             <div className="away__score">{countAway}</div>
           </div>
         </div>
-
         <BottomRow />
       </section>
       <section className="buttons">
@@ -34,14 +35,18 @@ function App() {
           {/* TODO STEP 4 - Now    we need to attach our state setter functions to click listeners. */}
           <button className="homeButtons__touchdown" onClick = { () => setHomeCount(countHome + 5)}>Home Touchdown</button>
           <button className="homeButtons__fieldGoal" onClick = { () => setHomeCount(countHome + 3)}>Home Field Goal</button>
+          <button className="awayButtons__reset" onClick = { () => setHomeCount(0)}>Home Reset</button>
         </div>
         <div className="awayButtons">
           <button className="awayButtons__touchdown" onClick = { () => setAwayCount(countAway + 5)}>Away Touchdown</button>
           <button className="awayButtons__fieldGoal" onClick = { () => setAwayCount(countAway + 3)}>Away Field Goal</button>
+          <button className="awayButtons__reset" onClick = { () => setAwayCount(0)}>Away Reset</button>
         </div>
       </section>
     </div>
   );
 }
+
+
 
 export default App;
